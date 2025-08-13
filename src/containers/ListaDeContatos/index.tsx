5ed2bcf92ca9a50b655b46d00cd480e8de987aba
Yuri Romeu <yuriromeu2007@gmail.com>
2025-08-13 10:48:25 -0300

    barra de pesquisa funcionando

--- src/containers/ListaDeContatos/index.tsx
+++ src/containers/ListaDeContatos/index.tsx
@@ -1,19 +1,35 @@
 import Contato from '../../Components/Contato';
 import { BotaoAdicionarContato, Container, Icon, ListaContatos, SearchInput } from './styles';
 import { useAppSelector } from '../../store/hooks';
+import { useState } from 'react';
 
 const ListaDeContatos = () => {
      const contatos = useAppSelector(state => state.contatos);
+
+     const [nomeBuscado, setNomeBuscado] = useState('');
+
+     const buscarNome = (e: React.ChangeEvent<HTMLInputElement>) => {
+          setNomeBuscado(e.target.value);
+     };
+
+     const contatosFiltrados = contatos.filter(contato =>
+          contato.nome.toLowerCase().includes(nomeBuscado.toLowerCase()),
+     );
 
      return (
           <Container>
                <div style={{ position: 'relative' }}>
                     <Icon />
-                    <SearchInput type="search" placeholder="Search" />
+                    <SearchInput
+                         type="search"
+                         placeholder="Search"
+                         value={nomeBuscado}
+                         onChange={buscarNome}
+                    />
                     <BotaoAdicionarContato>Add Contact</BotaoAdicionarContato>
                </div>
                <ListaContatos>
-                    {contatos.map(contato => (
+                    {contatosFiltrados.map(contato => (
                          <Contato key={contato.id} {...contato} />
                     ))}
                </ListaContatos>
